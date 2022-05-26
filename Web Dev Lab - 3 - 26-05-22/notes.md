69bb9dab4c4caf174e1dc33039481663e4f01274
raghunathan-r <50546911+raghunathan-r@users.noreply.github.com>
2022-05-26 10:34:51 +0530

    add web dev lab 3 files

---
+++ Web Dev Lab - 3 - 26-05-22/notes.md
@@ -0,0 +1,5 @@
+Q1 . num sum using prompt
+Q2 . num sum using text box
+Q3 . simple calculator
+Q4 . input limit of number from prompt and print numbers and squares of the number in an alert box
+    
---
+++ Web Dev Lab - 3 - 26-05-22/num_sum.js
@@ -0,0 +1,10 @@
+// prompt takes the input from the windows dialog box
+
+let a = prompt("enter a ? : ");
+let b = prompt("enter b ? : ");
+
+// findin the sum, we have to make the variables into integers
+var sum = parseInt(a) + parseInt(b);
+
+// display using an alert box
+alert("sum = " + sum);
